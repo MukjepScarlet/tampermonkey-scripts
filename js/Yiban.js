f5df9efd1716153a551d24b98d77ab1224c0c900
木葉 Scarlet <93977077+MukjepScarlet@users.noreply.github.com>
2023-05-30 18:29:57 +0800

    添加新的映射关系, 添加左右键切换上下题之功能

--- js/Yiban.js
+++ js/Yiban.js
@@ -1,70 +1,120 @@
 // ==UserScript==
 // @name         Yiban Resolve
-// @namespace    https://github.com/MukjepScarlet/tampermonkey-scripts/blob/main/
-// @run-at       document-end
-// @version      0.2
+// @namespace    http://tampermonkey.net/
+// @version      0.3
 // @description  Bypass!
 // @author       konohaScarlet_
-// @icon         https://www.yiban.cn/favicon.ico
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
 // @grant        none
 // @match        *://exam.yooc.me/group/*/exam/*
-// @original-license GPL License
 // ==/UserScript==
 
-(async function() {
+const decode = {
+    "=4WatRWYAN2bvlXXiMjIsISMiwiIwIyW": ["A", "B", "D"],
+    "=4WatRWYAN2bvlXXiMjIsIiMiwiIwIyW": ["A", "C", "D"],
+    "=4WatRWYAN2bvlXXiIjIsISMiwiIwIyW": ["A", "B", "C"],
+    "=4WatRWYAN2bvlXXiQjIsIiMiwiIwIyW": ["A", "C", "E"],
+    "=4WatRWYAN2bvlXXiMjIsIiMiwiIxIyW": ["B", "C", "D"],
+    "=4WatRWYAN2bvlXXiQjIsISMiwiIwIyW": ["A", "B", "E"],
+    "==gbp1GZhB0Yv9WedJSMiwiIwIyW": ["A", "B"],
+    "==gbp1GZhB0Yv9WedJiMiwiIwIyW": ["A", "C"],
+    "==gbp1GZhB0Yv9WedJiMiwiIxIyW": ["B", "C"],
+    "==gbp1GZhB0Yv9WedJyMiwiIxIyW": ["B", "D"],
+    "ulWbkFGQj92b51lIzICLiIjIsISMiwiIwIyW": ["A", "B", "C", "D"],
+    "ulWbkFGQj92b51lI0ICLiIjIsISMiwiIwIyW": ["A", "B", "C", "E"],
+    "ulWbkFGQj92b51lI0ICLiMjIsIiMiwiIwIyW": ["A", "C", "D", "E"],
+    "==gbp1GZhB0Yv9WedJCNiwiIzICLiIjIsISMiwiIwIyW": ["A", "B", "C", "D", "E"],
+    "S+xUEw7naejDmkN3dm8ESw==": "A",
+    "MXz/ccgKyTDm4S07GwdZSQ==": "B",
+    "LVXqNuIbrI4w1S9XcwBWxQ==": "C",
+    "B0jjbql+rpI89IcabXBtIw==": "D",
+    "lbMHM29yZx4xQ3i31EBQSw==": "E",
+
+    "ekB9RCpjA7nx6HLFU0S4fg==": "A",
+    "UwSNPvAWlrIKUaoSUwriYg==": "B",
+    "7x3PE4EgG6NoxX3LCMK4wA==": "C",
+    "vaY5L2z2gxedI4LkQv2w1g==": "D",
+    "vavVp23XuSjK8xbuKyfl1TchwQ71j44zrebljBy\/z1I=": ["A", "B", "C", "D", "E"],
+    "vavVp23XuSjK8xbuKyfl1Y6mgg6b7yzcgUto9rTBlms=": ["A", "B", "C", "D"],
+    "LE0pFroqYLH0DhSPvhpIbA==": ["A", "B", "C"],
+    "KOKP6em09Uqn5P13lIf8Kg==": ["A", "B", "D"],
+    "lKe/G3bBu2t/JiXIa9TSlA==": ["B", "C", "D"],
+    "NzXaQ8Zvzb1spWYZiaKMKQ==": ["A", "B"],
+    "ojbn01F+UgOOSERjnNnBwQ==": ["A", "C"],
+    "TCujI7LvF9+S7D5yeRyzWg==": ["B", "C"],
+    "XR17648FKif4ktUCZF9h0w==": ["B", "D"],
+    "wam44wmBSSNN4dPi8eZl4w==": ["C", "D"],
+
+    "pUsI4QDZBgi6a+MSlczzzC9g0o1iViM8WF5iFZwdANQ=": ["A", "B", "C", "D"],
+    "L/iKJ/gMhE3fBYXxVEn3iA==": ["A", "C", "D"],
+    "X0tFOdyxRmxLwcF+4Nu+jQ==": ["A", "B", "C"],
+    "I6zV3xcySnWk/kqy868riw==": ["A", "B"],
+    "vsAyq1dI1vDG1AZ+TOVZVQ==": ["A", "C"],
+    "RyaO9eiwz9Yd9fwVJHxsdQ==": ["A", "D"],
+};
+
+async function request(url, options) {
+    const response = await fetch(url, options);
+    const resJson = await response.json();
+    return resJson;
+}
+
+async function main() {
     'use strict';
-
-    const decode = {
-        "ulWbkFGQj92b51lIwIyW": "A",
-        "ulWbkFGQj92b51lIxIyW": "B",
-        "ulWbkFGQj92b51lIyIyW": "C",
-        "ulWbkFGQj92b51lIzIyW": "D",
-        "ulWbkFGQj92b51lI0IyW": "E",
-        "==gbp1GZhB0Yv9WedJSMiwiIwIyW": ["A", "B"],
-        "==gbp1GZhB0Yv9WedJiMiwiIwIyW": ["A", "C"],
-        "==gbp1GZhB0Yv9WedJyMiwiIwIyW": ["A", "D"],
-        "==gbp1GZhB0Yv9WedJiMiwiIxIyW": ["B", "C"],
-        "==gbp1GZhB0Yv9WedJyMiwiIxIyW": ["B", "D"],
-        "==gbp1GZhB0Yv9WedJyMiwiIyIyW": ["C", "D"],
-        "=4WatRWYAN2bvlXXiIjIsISMiwiIwIyW": ["A", "B", "C"],
-        "=4WatRWYAN2bvlXXiMjIsISMiwiIwIyW": ["A", "B", "D"],
-        "=4WatRWYAN2bvlXXiQjIsISMiwiIwIyW": ["A", "B", "E"],
-        "=4WatRWYAN2bvlXXiMjIsIiMiwiIwIyW": ["A", "C", "D"],
-        "=4WatRWYAN2bvlXXiQjIsIiMiwiIwIyW": ["A", "C", "E"],
-        "=4WatRWYAN2bvlXXiMjIsIiMiwiIxIyW": ["B", "C", "D"],
-        "ulWbkFGQj92b51lIzICLiIjIsISMiwiIwIyW": ["A", "B", "C", "D"],
-        "ulWbkFGQj92b51lI0ICLiIjIsISMiwiIwIyW": ["A", "B", "C", "E"],
-        "ulWbkFGQj92b51lI0ICLiMjIsIiMiwiIwIyW": ["A", "C", "D", "E"],
-        "==gbp1GZhB0Yv9WedJCNiwiIzICLiIjIsISMiwiIwIyW": ["A", "B", "C", "D", "E"]
-    };
-
-    async function request(url, options) {
-        const response = await fetch(url, options);
-        const resJson = await response.json();
-        return resJson;
-    }
 
     const examId = window.location.href.match(/exam\/([0-9]+)?/)[1];
     const userId = document.cookie.match(/user_id=([0-9]+)?/)[1];
-    const examuserId = (await request(`https://exambackend.yooc.me/api/exam/setting/get?examId=${examId}&userId=${userId}`)).data.examuserId;
 
-    const data = (await request(`https://exambackend.yooc.me/api/exam/paper/get?examuserId=${examuserId}`)).data
+    const token = document.cookie.split(';').find(cookie => cookie.includes('user_token')).split('=')[1];
+    const yiban_id = document.cookie.split(';').find(cookie => cookie.includes('yiban_id')).split('=')[1];
+    const examuserId = (await request(`https://exambackend.yooc.me/api/exam/setting/get?examId=${examId}&userId=${userId}&token=${token}&yibanId=${yiban_id}`)).data.examuserId;
+    const datas = (await request(`https://exambackend.yooc.me/api/exam/paper/get?examuserId=${examuserId}&token=${token}&yibanId=${yiban_id}`))
 
-    const answers = data.flatMap(sec => sec.subjects).map(subj => subj.answer = decode[subj.answer]);
+    const subjects = datas.data.map(sec => sec.subjects).reduce((acc, val) => acc.concat(val), []);
+    const answers = subjects.map(subj => decode[subj.answer] ?? "答案不在库中");
 
     const m = document.getElementsByClassName("jsx-3643416060").item(0).parentElement;
-
-    let ansTag = document.createElement("div");
+    const ansTag = document.createElement("div");
     ansTag.setAttribute("id", "answer");
     m.appendChild(ansTag);
 
     // 题号变化随时更新
-
     const questionNumTag = document.getElementsByClassName("jsx-3527395752").item(3);
 
     questionNumTag.addEventListener("DOMSubtreeModified", e => {
         const newValue = e.target.data; // 改变后（现在）的题号
-        const currentAnswer = answers[newValue - 1];
-        ansTag.innerHTML = `第${newValue}题答案：` + (typeof currentAnswer == "string" ? currentAnswer : (currentAnswer?.join("，") ?? "选项不在库中，请反馈"));
+        let currentAnswer = answers[newValue - 1];
+        if (typeof currentAnswer === "object") currentAnswer = currentAnswer.join("，");
+        ansTag.innerHTML = `第${newValue}题答案：${currentAnswer}`;
     });
-})();
+
+    // 0: Submit 1: Prev 2: Next
+    const buttons = document.getElementsByClassName("jsx-751469096");
+
+    document.addEventListener('keydown', event => {
+        // 按下右方向键
+        switch (event.keyCode) {
+            case 37: // Left
+                buttons.item(1).dispatchEvent(new MouseEvent('click', {
+                    bubbles: true,
+                    cancelable: true,
+                    view: window
+                }));
+                break;
+
+            case 39: // Right
+                buttons.item(2).dispatchEvent(new MouseEvent('click', {
+                    bubbles: true,
+                    cancelable: true,
+                    view: window
+                }));
+                break;
+        }
+    });
+};
+
+// 检测是否通过验证
+while (!document.getElementsByClassName("jsx-3643416060").item(0))
+    await new Promise(resolve => setTimeout(resolve, 100));
+
+main();
